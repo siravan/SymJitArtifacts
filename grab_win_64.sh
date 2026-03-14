fac2804e0adf9260ece508969475861dae240f90
Shahriar Iravanian <siravan@svtsim.com>
2026-03-14 07:59:18 -0400

    revert back to symjit 2.6.0

--- grab_win_64.sh
+++ grab_win_64.sh
@@ -8,7 +8,7 @@ for platform in linux-64 win-64 osx-64 osx-arm64
 do
     conda create --platform $platform -n $platform -c conda-forge -y --no-deps --no-default-packages
     conda activate $platform
-    conda install symjit=2.13.5 -n $platform -c conda-forge --no-deps -y
+    conda install symjit=2.6.0 -n $platform -c conda-forge --no-deps -y
     find ~/miniforge3/envs/$platform -name '_lib*' -exec mv {} . \;
     tar -czf symjit_$platform.tar.gz _lib* --mode=0666
     rm _lib*
